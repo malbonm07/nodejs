34068b56316184a03f889a1b84cab85fb9ab2f97
Miguel Alonso <malbonm07@gmail.com>
2019-09-22 11:08:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+const chalk = require('chalk');
+const validator = require('validator');
+const getData = require('./notes.js');
+
+const log = console.log
+// fs.writeFileSync('notes.txt', 'hola mundo desde notes.txt!');
+// try {
+//     fs.appendFileSync('notes.txt', 'data to append to data.txt');
+//     console.log('The "data to append" was appended to file!');
+//   } catch (err) {
+// }
+
+
+// console.log(validator.isEmail('miguel@email.com'))
+
+log(chalk.green('SUCCESS'))
