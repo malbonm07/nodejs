abe282811706dc077ad225085380467aeda96543
Miguel Alonso Marroquin <39355991+malbonm07@users.noreply.github.com>
2019-10-08 11:22:51 -0500

    Add files via upload

---
+++ playground/2-callback.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+    console.log("after 5 seconds.")
+}, 5000)
+
+const data = new Promise((resolve, reject) => {
+    
+})
+
+const geoconde = (address, callback) => {
+}
+
+const geo = geoconde("Perú")
+
+console.log(geo);
