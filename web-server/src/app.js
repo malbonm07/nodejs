c29a83a4409427f6dd4fc2c9c1055a49252e706f
Miguel Alonso <malbonm07@gmail.com>
2019-10-08 21:46:23 -0500

    added html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const path = require('path');
+
+console.log(__dirname)
+console.log(__filename)
+
+const pathOfPublicFolder = path.join(__dirname, '../public')
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello from express!')
+app.use(express.static(pathOfPublicFolder))
+
+// app.get('', (req, res) => {
+//     res.send()
+// })
+// app.get('/help', (req, res) => {
+//     res.send("<h1>This is the Help section</h1>")
+// })
+// app.get('/about', (req, res) => {
+//     res.send("<h1>This is the about section</h1>")
+// })
+app.get('/weather', (req, res) => {
+    res.send("<h1>This is the weather section</h1>")
 })
 
 app.listen('3000', () => {
